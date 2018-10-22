478d865f0136c888322a4ea6192d78ec13092e83
lukesnc <35816797+lukesnc@users.noreply.github.com>
2018-10-22 17:30:30 -0400

    pointer practice 10/23

--- PreseasonExercises/PointerPractice.cpp
+++ PreseasonExercises/PointerPractice.cpp
@@ -9,16 +9,16 @@ int main()
 	myPointer = &myVar; // & symbol gets a variables location in memory
 
 	cout << "Value of myVar variable: ";
-    cout << myVar << endl;
+        cout << myVar << endl;
 
-    // print the address stored in myPointer variable
-    cout << "Address stored in myPointer variable: ";
-    cout << myPointer << endl;
+        // print the address stored in myPointer variable
+        cout << "Address stored in myPointer variable: ";
+        cout << myPointer << endl;
 
-    // access the value at the address available in pointer
-    cout << "Value of *myPointer variable: ";
-    cout << *myPointer << endl;
+        // access the value at the address available in pointer
+        cout << "Value of *myPointer variable: ";
+        cout << *myPointer << endl;
 
-    system("pause");
+        system("pause");
 	return 0;
 }
