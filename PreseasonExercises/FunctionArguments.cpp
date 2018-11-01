79979b453712b24b0e6314cf114db399fcb37007
lukesnc <35816797+lukesnc@users.noreply.github.com>
2018-11-01 12:52:38 -0700

    function args and string concat 11/1

--- PreseasonExercises/FunctionArguments.cpp
+++ PreseasonExercises/FunctionArguments.cpp
@@ -10,12 +10,15 @@ string putTogether(string a, string b)
 int main()
 {
 	string user1, user2;
-	cout << "Enter 2 strings, seperated by the ENTER key: ";
-	cin >> user1;
-	cin >> user2;
+	cout << "Enter a strings, then press ENTER: ";
+	getline(cin, user1); 
+	// getline is similar to cin, except it allows the user to put spaces
+	// as the name suggests, it grabs what's in the entire line
+	cout << "Enter another string: ";
+	getline(cin, user2);
 	
 	// the function puts the 2 together, so just print the result of the function
-	cout << putTogether(user1, user2);
+	cout << "Resulting string = " << putTogether(user1, user2);
 
 	return 0;
 }
